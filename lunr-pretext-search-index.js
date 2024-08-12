bcd9c941095e2216321d03a78cbabb4de1c9d173
John Estes <59932367+johnestesmath@users.noreply.github.com>
2024-08-12 01:24:01 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,329 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter-2",
+  "level": "1",
+  "url": "frontmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  My Website   copyright  "
+},
+{
+  "id": "sec-mission",
+  "level": "1",
+  "url": "sec-mission.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Mission Statement",
+  "body": " Mission Statement  The Belhaven Maker Campus is a collaborative partnership between the Art & Design and Math & Computer Science Departments to provide an environment that inspires creativity and critical thinking through use of constructive technology.  The Belhaven Maker Campus has the following stated goals:   Develop and maintain a maker culture among the student body    Provide cross-departmental opportunities with a focus on a STEAM (Science, Technology, Engineering, Art, and Mathematics) mentality    Provide access to current and traditional tools and methods of production    Promote the creation of unique projects both student-led and professionally funded    Increase awareness about how the Biblical Worldview intersects with contemporary material production    Provide skill-training in emerging technology and processes    Inspire students to engage culture creatively and effectually through making    Ground students in traditional thought and practice while motivating them to find ways to personally engage with the most contemporary aspects of their chosen field of study.     "
+},
+{
+  "id": "sec-makerculture",
+  "level": "1",
+  "url": "sec-makerculture.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Maker Culture",
+  "body": " Maker Culture  The Maker Culture (aka Maker Movement) is a rapidly growing social movement centered around the idea of physical production. It is a loose confederation comprised of artists, scientists, and laborers from various trades. Maker communities focus on hands-on learning and the creative incorporation of current technology to better society. Open source and collaborative, this movement seeks to increase access to current technology, ideas, and resources for everyone.  This movement is an exercise in sharing and culture-building. Methodologically the Biblical Worldview, with its focus on service and charity, can: 1) find ways to connect with and evangelize within the Maker Movement and 2) collaborate with others to improve the general condition of those outside the movement.      "
+},
+{
+  "id": "vid-makerculture",
+  "level": "2",
+  "url": "sec-makerculture.html#vid-makerculture",
+  "type": "Figure",
+  "number": "1.2.1",
+  "title": "",
+  "body": "   "
+},
+{
+  "id": "sec-faculty",
+  "level": "1",
+  "url": "sec-faculty.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Faculty Advisors",
+  "body": " Faculty Advisors  The faculty advisor’s role is to facilitate projects, research opportunities, provide skill and safety training, facilitate events and work with the administration to enhance member experience. Current Faculty Advisors:         Mr. Nate Theisen Co-Chair and Professor of Art & Design Faculty Page          Dr. John Estes Dean of the School of Science & Mathematics, Professor and Yerger Chair of Mathematics and Computer Science, and Director of Engineering Partnerships. Faculty Page   "
+},
+{
+  "id": "sec-faculty-3-1",
+  "level": "2",
+  "url": "sec-faculty.html#sec-faculty-3-1",
+  "type": "Figure",
+  "number": "1.3.1",
+  "title": "",
+  "body": "   "
+},
+{
+  "id": "sec-faculty-4",
+  "level": "2",
+  "url": "sec-faculty.html#sec-faculty-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Mr. Nate Theisen "
+},
+{
+  "id": "sec-faculty-5-1",
+  "level": "2",
+  "url": "sec-faculty.html#sec-faculty-5-1",
+  "type": "Figure",
+  "number": "1.3.2",
+  "title": "",
+  "body": "   "
+},
+{
+  "id": "sec-faculty-6",
+  "level": "2",
+  "url": "sec-faculty.html#sec-faculty-6",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Dr. John Estes "
+},
+{
+  "id": "sec-membership",
+  "level": "1",
+  "url": "sec-membership.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Membership",
+  "body": " Membership  Belhaven University students may join the Maker Campus by paying a $20 annual fee. Email one of the Faculty Advisors or scan the QR code located in the labs to join.  Members of the Maker Campus obtain card access to the labs by completing a lab orientation for each respective space.   Benefits of Membership   "
+},
+{
+  "id": "sec-safety",
+  "level": "1",
+  "url": "sec-safety.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Safety",
+  "body": " Safety  No Lab equipment can be used without proper safety training. This information is included at orientation and training sessions. (see “Training” above)  Each Lab is provided with the appropriate safety equipment, first aid, and fire suppression appropriate for its machines and processes.  In the event of a severe injury or fire: 911 must be called and Belhaven Security notified (601) 968-5900.  "
+},
+{
+  "id": "sec-skills",
+  "level": "1",
+  "url": "sec-skills.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Skills",
+  "body": " Skills  Members of the Maker Campus will have opportunities to learn lab equipment and software through courses, faculty-led projects, or training seminars conducted by either the faculty or a trained and authorized member.  Currently supported fabrication methods include   3D Printing (FDM and SLA)    laser cutting and engraving    vinyl cutting    robotic drawing    robotic pottery    electronics building    CNC machining in wood and metal    vacuum forming    basic wood and metal shop processes    soldering     Training is basic robotics is available by joining the Maker Campus Robotics Crew (a subset of the Maker Campus).  "
+},
+{
+  "id": "sec-badges",
+  "level": "1",
+  "url": "sec-badges.html",
+  "type": "Section",
+  "number": "3.3",
+  "title": "Nametags &amp; Badges",
+  "body": " Nametags & Badges  The Maker Campus operates according to a badge system.        "
+},
+{
+  "id": "sec-badges-3-1",
+  "level": "2",
+  "url": "sec-badges.html#sec-badges-3-1",
+  "type": "Figure",
+  "number": "3.3.1",
+  "title": "",
+  "body": "   "
+},
+{
+  "id": "sec-3dprinting",
+  "level": "1",
+  "url": "sec-3dprinting.html",
+  "type": "Section",
+  "number": "3.4",
+  "title": "3D Printing",
+  "body": " 3D Printing  3D printing is an additive manufacturing process which creates 3-dimensional objects from digital files. This is done by ‘slicing’ three dimensional models into layers using slicing software, then printing them out in layers using a printer.  The models are created using CAD (Computer Aided Design) software. The two most common commercially available types of 3D printing are FDM and SLA.   FDM Printing  Fused Deposition Modeling (FDM) utilized thermoplastic filament which is heated, extruded, and put down in layers on a bed. FDM printing is an excellent choice for most common prints.    SLA Printing  Stereolithography (SLA) printing uses UV light (typically a laser) to convert resin into a solid plastic. This is also done in layers, but the bed is inverted, and the model is pulled upward out of a vat of resin. SLA printing is an excellent choice to pieces with small details or complex geometries such as figurines.    Maker Campus Availability   Design Software: AutoCAD, TinkerCAD, Blender, Fusion360, Oculus Medium, Meshroom, Adobe Project Neo, OnShape.   Slicing Software: Prusa Slicer, Cura, Flashprint, Chitubox, and Photon Workshop, Canvas   Hardware: FLM printers (filament-based), SLA printers, 3D scanners and Virtual Reality (see “Labs and Equipment” below for specific hardware)   "
+},
+{
+  "id": "subsec-3dpavailability-2",
+  "level": "2",
+  "url": "sec-3dprinting.html#subsec-3dpavailability-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Design Software: "
+},
+{
+  "id": "subsec-3dpavailability-3",
+  "level": "2",
+  "url": "sec-3dprinting.html#subsec-3dpavailability-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Slicing Software: "
+},
+{
+  "id": "subsec-3dpavailability-4",
+  "level": "2",
+  "url": "sec-3dprinting.html#subsec-3dpavailability-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Hardware: "
+},
+{
+  "id": "sec-engraving",
+  "level": "1",
+  "url": "sec-engraving.html",
+  "type": "Section",
+  "number": "3.5",
+  "title": "Digital Cutting and Engraving",
+  "body": " Digital Cutting and Engraving  The Maker Campus offers several options when it comes to cutting and engraving items. Vinyl cutting is a process which uses 2D vector design software and a computer-controlled cutting machine to cut out designs using a metal blade for use as signs, stickers, or heat transfer. Laser cutters and engravers also begin with vector design software, but instead of a blade, it uses a high-powered focused beam of light to cut or engrave designs into materials such as wood, plastic, and leather. CNC (Computer Numerical Control) machines use computers to control machine devices such as routers, and plasma cutters to cut and\/or engrave flat or sheet materials. CNC machines use CAM (Computer Aided Machining) software to direct machine output.   Maker Campus Availability   Software: Adobe Illustrator (Art + Design Majors), Inkscape, Cricut, Vinyl Master Pro, vCarve, Carbide Create, Adobe Suite, Glowforge, FireCAM, Fire Control   Hardware: Wood CNC machines, laser cutter, a vinyl cutter, Cricut, CNC plasma cutter. (see “Labs and Equipment” below for specific machines.)   "
+},
+{
+  "id": "subsec-engravingavailability-2",
+  "level": "2",
+  "url": "sec-engraving.html#subsec-engravingavailability-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Software: "
+},
+{
+  "id": "subsec-engravingavailability-3",
+  "level": "2",
+  "url": "sec-engraving.html#subsec-engravingavailability-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Hardware: "
+},
+{
+  "id": "sec-electronics",
+  "level": "1",
+  "url": "sec-electronics.html",
+  "type": "Section",
+  "number": "3.6",
+  "title": "Electronics Building",
+  "body": " Electronics Building  Electronics connects the digital world with the natural world through various sensory inputs. It creates and connects devices which control and amplify electric currents used by electronic devices and circuitry. The control of such devices is done through coding.   Maker Campus Availability   Software: Arduino IDE, Python, Processing, Atom, and previously mentioned software   Hardware: PC’s, Arduino Unos and Nanos, Raspberry Pi, soldering iron and tools, miscellaneous sensors, machine parts, and robotics kits   "
+},
+{
+  "id": "subsec-electronicsavailability-2",
+  "level": "2",
+  "url": "sec-electronics.html#subsec-electronicsavailability-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Software: "
+},
+{
+  "id": "subsec-electronicsavailability-3",
+  "level": "2",
+  "url": "sec-electronics.html#subsec-electronicsavailability-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Hardware: "
+},
+{
+  "id": "sec-other",
+  "level": "1",
+  "url": "sec-other.html",
+  "type": "Section",
+  "number": "3.7",
+  "title": "Other Machines &amp; Processes",
+  "body": " Other Machines & Processes  The Maker Campus is constantly evolving and expanding, below is a listing of just some of the current fabrication methods available.   Maker Campus Availability   Software: Inkscape, Adobe (Art + Design Majors), OnShape, Fusion 360, AutoCAD, Oculus Medium, Gravity Sketch, Blender, etc. (see more above)   Hardware: Robotic Drawing, Robotic Pottery, Vacuum Forming, woodshop, metal shop (see “Labs and Equipment” below)   "
+},
+{
+  "id": "subsec-otheravailability-2",
+  "level": "2",
+  "url": "sec-other.html#subsec-otheravailability-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Software: "
+},
+{
+  "id": "subsec-otheravailability-3",
+  "level": "2",
+  "url": "sec-other.html#subsec-otheravailability-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Hardware: "
+},
+{
+  "id": "sec-equipment",
+  "level": "1",
+  "url": "sec-equipment.html",
+  "type": "Section",
+  "number": "3.8",
+  "title": "Equipment Usage",
+  "body": " Equipment Usage  Members may only use the equipment that they have been trained in. (see “Training” above and Appendix B)   Handling  Members may only use consumable lab materials with permission. Training is required before use rights are granted.    Purchasing The Faculty Advisors do their best to provide consumables such as PLA filament and UV resin, wood, etc. Beyond what is provided in the lab, materials (especially if exotic) will be purchased by the member using it.   Clean Up  Follow all clean-up procedures learned during training. In general, all filament rolls must be put back into the correct air-tight bin, all resin printers closed, all resin cleaned off, all surfaced cleaned, and floors swept if appropriate to the mess.  Furthermore, all tools must be put away and equipment powered down after use.  Membership may be revoked if clean-up is continuously avoided.   "
+},
+{
+  "id": "sec-serviceproj",
+  "level": "1",
+  "url": "sec-serviceproj.html",
+  "type": "Section",
+  "number": "4.1",
+  "title": "Service Projects",
+  "body": " Service Projects  Throughout the year, the Maker Campus is approached by various individuals and groups within and outside Belhaven University for service projects. These projects are voluntary, facilitated by a Faculty Advisor, and led by one or more members.  "
+},
+{
+  "id": "sec-inidvproj",
+  "level": "1",
+  "url": "sec-inidvproj.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Individual Projects",
+  "body": " Individual Projects  A member of the Belhaven Maker Campus may submit a project request with an estimated timeline and list of supplies. If accepted by a faculty advisor, the member can work on the project with priority to machines and may solicit the aid of other members.  "
+},
+{
+  "id": "sec-library",
+  "level": "1",
+  "url": "sec-library.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Library 200 Lab",
+  "body": " Library 200 Lab  Located on the second floor of the library. The Library 200 Lab includes the following:     Workstations    3D Printers (FLM):   Original Prusa i3 mks 4 (x4)    Bambu Labs X1 Carbon    Original Prusa XL    Creality Ender 3 Pro 3       Lab PC    Tools and hardware    soldering stations    Arduino (Unos, Nanos, Megas)     "
+},
+{
+  "id": "sec-fitzhugh",
+  "level": "1",
+  "url": "sec-fitzhugh.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Fitzhugh 300 Lab",
+  "body": " Fitzhugh 300 Lab  Located on the third floor of the Fitzhugh. The Fitzhugh 300 Lab includes six lab PC's with the following software:     AutoCAD and Fusion 360    Inventor    Mathematica    Arduino IDE    Processing    R    print slicers     "
+},
+{
+  "id": "sec-bitsy",
+  "level": "1",
+  "url": "sec-bitsy.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Bitsy Irby Labs",
+  "body": " Bitsy Irby Labs  Bitsy Irby boasts two lab spaces of the Maker Campus located on east wing of the first floor. These labs include the following.   Bitsy-Irby 131: 3D Fabrication Lab     4 Workstations and one Large Station    FLM 3D Printing: 4 Flashforge Finders, 1 Flashforge Finder version 3, Lulzbot Workhorse Printer with Pallette 2 Multifilament Control Hub, 2 Artillery Sidewinders, and 1 FLSUN Super Racer    SLA 3D Printing: Peopoly Phenom, Elegoo Mars 5 Ultra, Anycubic Photon M3, Elegoo Mercury XS Wash and Curing Station    Computing\/Designing: 2 Custom Built PCs    Cutting and Engraving: Glowforge, XL Vinyl Cutting Machine, Cricut Maker 3    Virtual Sculpting 2 Oculus Rift S Headsets with Oculus Medium Software    3D Scanning: Einscan-SE, Creality Scan Lizard       Bitsy Irby 126: Sculpture Studio  Shapeoko XXL CNC Router 3018 CNC Machine Woodshop tools (Saws, Planers, Lathe, etc.) Metal Shop (welders, saws, benders, grinders, etc.) Post-production and clean-up equipment Formech Vacuum Former Cross Fire Pro Plasma CNC 3D Potter      AutoCAD and Fusion 360    Inventor    Mathematica    Arduino IDE    Processing    R    print slicers     "
+},
+{
+  "id": "sec-labrules",
+  "level": "1",
+  "url": "sec-labrules.html",
+  "type": "Section",
+  "number": "5.4",
+  "title": "Lab Rules",
+  "body": " Lab Rules  Need to be updated.  "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
